2a2a0eb6a1b414a94fc60e04074c142bdd08c76c
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-07-16 11:45:21 +0200

    add send email gamil

--- project/settings.py
+++ project/settings.py
@@ -132,6 +132,17 @@ MEDIA_ROOT= BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 AUTHENTICATION_BACKENDS=['accounts.backend.Login_Email_UserName']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'mohamedelazab2017@gmail.com'
+EMAIL_HOST_PASSWORD = 'tusctkkyomqhhsrg'
+
+EMAIL_USE_SSL = False
