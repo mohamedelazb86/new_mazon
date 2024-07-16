27c3be617c5bbf999b2bce6002393640204b13d5
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-07-16 12:03:04 +0200

    add bootstarap5

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my package
+
+    "django_bootstrap5",
 ]
 
 MIDDLEWARE = [
